5df1d10bf059450643ba338e10a92b55d2a95b63
tipharez-allmighty <artemkolos@gmail.com>
2024-10-31 19:24:57 +0800

    timeout 20 sec

--- llm_api/views.py
+++ llm_api/views.py
@@ -245,5 +245,5 @@ class ChatAPIViewTest(APIView):
                 'response': ai_message
             }
         }
-        time.sleep(30)
+        time.sleep(20)
         return Response({"received_data": message_json}, status=status.HTTP_200_OK)
