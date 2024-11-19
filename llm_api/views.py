55670a167abee6719fa06ba22dfb2ed1c3d13c47
tipharez-allmighty <artemkolos@gmail.com>
2024-11-20 05:43:42 +0800

    Final Version

--- llm_api/views.py
+++ llm_api/views.py
@@ -1,24 +1,23 @@
-import requests
 import re
 import time
+import os
 
-from dotenv import load_env
-from django.shortcuts import render
+from dotenv import load_dotenv
 from django.http import HttpResponse
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .llm_base import Chat
+from llm_api.prompts import template, chat_data
 
+
+
+load_dotenv()
+
+ENDPOINT_URL = os.environ['ENDPOINT_URL']
+HUGGINGFACEHUB_API_TOKEN = os.environ['HUGGINGFACEHUB_API_TOKEN']
 
 def system_prompt_view(request):
-    json_url = "https://crazytweaks.online/api/aichatnew/example.json"
-    response = requests.get(json_url)
-    chat_data = response.json()
-
-    template_url = "https://crazytweaks.online/api/aichatnew/sys_template.txt"
-    response = requests.get(template_url)
-    template = response.text.strip()
 
     def format_chat_history(chat_history):
         formatted_messages = []
@@ -54,14 +53,11 @@ def system_prompt_view(request):
     sys_propmpt = replace_placeholders(template, chat_data)
     current_message = chat_data["current_message_text"]
     
-    ENDPOINT_URL_GOOD = 'https://ajouebfzyws0s5bt.us-east-1.aws.endpoints.huggingface.cloud/v1/chat/completions'
-    HUGGINGFACEHUB_API_TOKEN = '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'
-    
     ai_message = Chat(system_prompt=sys_propmpt,
                     message=current_message,
-                    endpoint_url=ENDPOINT_URL_GOOD,
+                    endpoint_url=ENDPOINT_URL,
                     api_token=HUGGINGFACEHUB_API_TOKEN).query()
-    
+    print(ai_message)
     assistant_response = ai_message['choices'][0]['message']['content']
     prompt_tokens = ai_message['usage']['prompt_tokens']
     completion_tokens = ai_message['usage']['completion_tokens']
@@ -82,21 +78,8 @@ def system_prompt_view(request):
         
 class ChatAPIView(APIView):
     def post(self, request):
-        chat_data = request.data
-        template_url = "https://crazytweaks.online/api/aichatnew/sys_template.txt"
-        response = requests.get(template_url)
-        template = response.text.strip()
-        switch = requests.get("https://crazytweaks.online/api/aichatnew/endpoint.json").json()
-        
+        chat_data = request.data        
         api_key = chat_data['api_key']
-        
-        if api_key != '3Gg8b6H2fQ9m1JzT':
-            return Response({
-                "received_data": {
-                    'api_key': api_key,
-                    'model_response': 'API key is not valid.'
-                }
-            }, status=status.HTTP_401_UNAUTHORIZED)  
         
         def format_chat_history(chat_history):
             formatted_messages = []
@@ -125,27 +108,13 @@ class ChatAPIView(APIView):
         current_message = chat_data["current_message_text"]
         chat_id = chat_data["chat_history"]["chat_id"]
         
-        
-        
-        ENDPOINT_URL_GOOD = 'https://ajouebfzyws0s5bt.us-east-1.aws.endpoints.huggingface.cloud/v1/chat/completions'
-        ENDPOINT_URL_BAD = 'https://n4mfh6a4olu13mvg.us-east-1.aws.endpoints.huggingface.cloud/v1/chat/completions'
-        HUGGINGFACEHUB_API_TOKEN = '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'
-        
-        if switch['endpoint'] == "ohuenno":    
-            ai_message = Chat(system_prompt=sys_propmpt,
+        ai_message = Chat(system_prompt=sys_propmpt,
                             message=current_message,
-                            endpoint_url=ENDPOINT_URL_GOOD,
+                            endpoint_url=ENDPOINT_URL,
                             api_token=HUGGINGFACEHUB_API_TOKEN).query()
             
-            ai_message = ai_message['choices'][0]['message']['content']
-        else:
-            ai_message = Chat(system_prompt=sys_propmpt,
-                            message=current_message,
-                            endpoint_url=ENDPOINT_URL_BAD,
-                            api_token=HUGGINGFACEHUB_API_TOKEN).query()
-            
-            ai_message = ai_message['choices'][0]['message']['content']    
-        
+        ai_message = ai_message['choices'][0]['message']['content']
+
         message_json = {
             'api_key': api_key,
             'model_response': {
@@ -160,20 +129,7 @@ class ChatAPIView(APIView):
 class ChatAPIViewTest(APIView):
     def post(self, request):
         chat_data = request.data
-        template_url = "https://crazytweaks.online/api/aichatnew/sys_template.txt"
-        response = requests.get(template_url)
-        template = response.text.strip()
-        switch = requests.get("https://crazytweaks.online/api/aichatnew/endpoint.json").json()
-        
         api_key = chat_data['api_key']
-        
-        if api_key != '3Gg8b6H2fQ9m1JzT':
-            return Response({
-                "received_data": {
-                    'api_key': api_key,
-                    'model_response': 'API key is not valid.'
-                }
-            }, status=status.HTTP_401_UNAUTHORIZED)  
         
         def format_chat_history(chat_history):
             formatted_messages = []
@@ -201,28 +157,14 @@ class ChatAPIViewTest(APIView):
         sys_propmpt = replace_placeholders(template, chat_data)
         current_message = chat_data["current_message_text"]
         chat_id = chat_data["chat_history"]["chat_id"]
+  
+        ai_message = Chat(system_prompt=sys_propmpt,
+                        message=current_message,
+                        endpoint_url=ENDPOINT_URL,
+                        api_token=HUGGINGFACEHUB_API_TOKEN).query()
         
-        
-        
-        ENDPOINT_URL_GOOD = 'https://ajouebfzyws0s5bt.us-east-1.aws.endpoints.huggingface.cloud/v1/chat/completions'
-        ENDPOINT_URL_BAD = 'https://n4mfh6a4olu13mvg.us-east-1.aws.endpoints.huggingface.cloud/v1/chat/completions'
-        HUGGINGFACEHUB_API_TOKEN = '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'
-        
-        if switch['endpoint'] == "ohuenno":    
-            ai_message = Chat(system_prompt=sys_propmpt,
-                            message=current_message,
-                            endpoint_url=ENDPOINT_URL_GOOD,
-                            api_token=HUGGINGFACEHUB_API_TOKEN).query()
-            
-            ai_message = ai_message['choices'][0]['message']['content']
-        else:
-            ai_message = Chat(system_prompt=sys_propmpt,
-                            message=current_message,
-                            endpoint_url=ENDPOINT_URL_BAD,
-                            api_token=HUGGINGFACEHUB_API_TOKEN).query()
-            
-            ai_message = ai_message['choices'][0]['message']['content']    
-        
+        ai_message = ai_message['choices'][0]['message']['content']
+ 
         message_json = {
             'api_key': api_key,
             'model_response': {
